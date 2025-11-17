16a6db7bf29b01f7443acf4608ab706f6e9a031c
Xiaorui Wang <xiaorui.wang@usi.ch>
2025-11-17 21:10:37 +0100

    feat: reverse integer

--- 7_reverse_integer.py
+++ 7_reverse_integer.py
@@ -1,23 +1,25 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        negative = x < 0
+        negative: bool = x < 0
         if negative:
             x = -x
         reversed = []
         while x % 10 != 0 or x // 10 != 0:
-            if x % 10 != 0 or len(reversed) > 0:
-                reversed.append(x % 10)
+            r = x % 10
             x = x // 10
+            if r == 0 and len(reversed) == 0:
+                continue
+            reversed.append(r)
         exponential = 0
         result = 0
         while len(reversed) != 0:
             result += reversed.pop() * (10 ** exponential)
+            if result > 2**31 - 1 or result <= -2**31:
+                return 0
             exponential += 1
 
         result = -result if negative else result
-        if result > 2**31 - 1 or result <= -2**31:
-            return 0
-
+        
         return result
 
 s = Solution()
