cfcc0782428f3346244273b9554a853e446c0954
Xiaorui Wang <xiaorui.wang@usi.ch>
2025-11-20 20:16:00 +0100

    feat: optimization

--- 26_remove_Duplicates_from_Sorted_Array.js
+++ 26_remove_Duplicates_from_Sorted_Array.js
@@ -2,23 +2,39 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var removeDuplicates = function (nums) {
+//     let fast = 0, slow = 0;
+//     const s = new Set();
+//     while (fast < nums.length) {
+//         const e = nums[fast];
+//         if (s.has(e)) {
+//             ++fast;
+//         } else {
+//             nums[slow] = e;
+//             s.add(e);
+//             ++slow;
+//             ++fast;
+//         }
+//     }
+//     return slow;
+// };
+
 var removeDuplicates = function (nums) {
-    let fast = 0, slow = 0;
-    const s = new Set();
-    while (fast < nums.length) {
-        const e = nums[fast];
-        if (s.has(e)) {
-            ++fast;
-        } else {
-            nums[slow] = e;
-            s.add(e);
-            ++slow;
-            ++fast;
-        }
+  if (nums.length === 1) {
+    return 1;
+  }
+  let fast = 1, slow = 1;
+  while (fast < nums.length) {
+    const e = nums[fast];
+    if (nums[fast] !== nums[fast - 1]) { // use this way to check if it's a different element
+      nums[slow] = nums[fast];
+      ++slow;
     }
-    return slow;
+    ++fast;
+  }
+  return slow;
 };
 
-let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+let nums = [0, 1, 1];
 
 console.log(`k: ${removeDuplicates(nums)}, nums: ${nums}`);
