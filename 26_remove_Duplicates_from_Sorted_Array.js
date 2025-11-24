8eb156462acb21be7add073e612b881cd40c7470
Xiaorui Wang <xiaorui.wang@usi.ch>
2025-11-24 22:42:03 +0100

    feat: optimization

--- 26_remove_Duplicates_from_Sorted_Array.js
+++ 26_remove_Duplicates_from_Sorted_Array.js
@@ -19,14 +19,30 @@
 //     return slow;
 // };
 
+// var removeDuplicates = function (nums) {
+//   if (nums.length === 1) {
+//     return 1;
+//   }
+//   let fast = 1, slow = 1;
+//   while (fast < nums.length) {
+//     if (nums[fast] !== nums[fast - 1]) { // use this way to check if it's a different element
+//       nums[slow] = nums[fast];
+//       ++slow;
+//     }
+//     ++fast;
+//   }
+//   return slow;
+// };
+
 var removeDuplicates = function (nums) {
   if (nums.length === 1) {
     return 1;
   }
-  let fast = 1, slow = 1;
+  let fast = 1,
+    slow = 1;
   while (fast < nums.length) {
-    const e = nums[fast];
-    if (nums[fast] !== nums[fast - 1]) { // use this way to check if it's a different element
+    if (nums[fast] !== nums[slow - 1]) {
+      // use this way to check if it's a different element
       nums[slow] = nums[fast];
       ++slow;
     }
@@ -35,6 +51,6 @@ var removeDuplicates = function (nums) {
   return slow;
 };
 
-let nums = [0, 1, 1];
+let nums = [0, 1, 1, 1, 2, 2, 2, 2,3,4,5];
 
 console.log(`k: ${removeDuplicates(nums)}, nums: ${nums}`);
